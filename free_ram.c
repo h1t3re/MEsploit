a0c8c5b4d30fed8737db16b0473f6f63f23edf29
h1t3re <lahbabi.chems.eddine@gmail.com>
2024-02-13 23:29:49 +0100

    Delete free_ram.c

--- free_ram.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()
-{
-	char *data = (char *)malloc((5828308+3978784)*sizeof(char));
-	memset((void *)data, 0, (5828308+3978784)*sizeof(char));
-	return 0;
-}
