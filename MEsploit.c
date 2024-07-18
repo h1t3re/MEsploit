fc9a85c076228796e182525ce5f0fa078047942d
h1t3re <lahbabi.chems.eddine@gmail.com>
2024-07-18 11:27:02 +0000

    Update of network scan

--- MEsploit.c
+++ MEsploit.c
@@ -10,8 +10,9 @@ int main()
 	network_configuration *interface;
 	char *interface_number;
 	char *ip_num;
-	char *ip;
-	char *ip0;
+	char **ip0;
+	char **ip1;
+	char *tmp0;
 	char *number = (char *)malloc(2*sizeof(char));
 	char *machine_number;
 	char *ip_number;
@@ -30,20 +31,27 @@ int main()
 		i = i +1;
 	
 	}
+	i = 0;
 	printf("Choose the ip address of the network to scan:\n");
 	ip_num = read_io();
 	num = atoi(ip_num);
-	ip0 = strsplit(strdup(net_config[int_number-1]->ip_addr[num]), strdup(".\0"));
-	ip = strconcatenate(strdup(ip0[0]), strdup(".\0"));
-	printf("%s%s\n", ip0[0], ip0[1]);
-	ip = strconcatenate(strdup(ip0[2]), strdup(".\0"));
-	ip = strconcatenate(strdup(ip0[4]), strdup(".\0"));
+	printf("%d\n", num);
+	ip0 = strsplit(strdup(net_config[int_number-1]->ip_addr[num]), strdup("/\0"));
+	printf("%s\n", ip0[0]);
+	ip1 = strsplit(strdup(ip0[0]), strdup(".\0"));
+	printf("%s%s%s%s%s%s%s%s\n", ip1[0], ip1[1], ip1[2], ip1[3], ip1[4], ip1[5], ip1[6], ip1[7]);
+	tmp0 = strconcatenate(strdup(ip0[0]), strdup(".\0"));
+	tmp0 = strconcatenate(strdup(tmp0), strdup(ip0[2]));
+	tmp0 = strconcatenate(strdup(tmp0), strdup(".\0"));
+	tmp0 = strconcatenate(strdup(tmp0), strdup(ip0[4]));
+	tmp0 = strconcatenate(strdup(tmp0), strdup(".\0"));
+	printf("%s\n", tmp0);
 	for(int i = 0; i < 255; i++)
 	{
 		snprintf(number, 2*sizeof(char), "%d\0", i);
-		ip = strconcatenate(ip, strdup(number));
-		printf("Scanning ip address: %s\n", ip);
-		forge_arp_packet(strdup("ff:ff:ff:ff:ff:ff\0"), strdup("ff:ff:ff:ff:ff:ff\0"), strdup(net_config[int_number-1]->ip_addr[num]), strdup(ip));
+		tmp0 = strconcatenate(strdup(tmp0), strdup(number));
+		printf("Scanning ip address: %s\n", tmp0);
+		forge_arp_packet(strdup("ff:ff:ff:ff:ff:ff\0"), strdup("ff:ff:ff:ff:ff:ff\0"), strdup(net_config[int_number-1]->ip_addr[num]), strdup(tmp0));
 	}
 	return 0;
 }
